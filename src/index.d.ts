547d54170caf78205a9f52a819c9c1fdf2dc2ba2
Mahavatar5D <mahavatar5d@gmail.com>
2021-02-05 01:12:07 -0300

    Initial commit.

---
+++ src/index.d.ts
@@ -0,0 +1,1 @@
+export declare let myAdd: (x: number, y: number) => number;
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export let myAdd: (x: number, y: number) => number = function (
+  x: number,
+  y: number
+): number {
+  return x + y;
+};
+
+console.log(myAdd(1,2))
