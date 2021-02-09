1707381bb8db1e8f631b1b97dfdf2093b5bb653e
Mahavatar5D <mahavatar5d@gmail.com>
2021-02-09 14:27:49 -0300

    Add compose funtion exemple.

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,26 @@ export let myAdd: (x: number, y: number) => number = function (
   return x + y;
 };
 
-console.log(myAdd(1,2))
+console.log(myAdd(1, 2))
+
+function compose<A, B, C>(f: (arg: A) => B, g: (arg: B) => C): (arg: A) => C {
+  return (x) => g(f(x));
+}
+
+interface Box<T> {
+  value: T;
+}
+
+function makeArray<T>(x: T): T[] {
+  return [x];
+}
+
+function makeBox<U>(value: U): Box<U> {
+  return { value };
+}
+
+const makeBoxedArray = compose(makeArray, makeBox);
+
+const result = makeBoxedArray("hello!").value[0].toUpperCase();
+
+console.log(result)
